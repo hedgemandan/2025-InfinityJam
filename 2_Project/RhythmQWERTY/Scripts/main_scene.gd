f0dbeb388633739ad56cfec384bdc69ba508f8b4
ThrillerOfAutumn <thrillerofautumn@gmail.com>
2025-05-16 14:56:22 +0100

    Some stuff happened

--- 2_Project/RhythmQWERTY/Scripts/main_scene.gd
+++ 2_Project/RhythmQWERTY/Scripts/main_scene.gd
@@ -5,7 +5,7 @@ var popup_instance = null
 var button = preload("res://Scenes/button.tscn")
 var button_instance = null
 @onready var marker = $Marker2D
-@onready var button_array = [$Button3, $Button4]
+@onready var button_array = [$Button3, $Button4, $Button5, $Button6, $Button7, $Button8, $Button9, $Button10, $Button11, $Button12, $Button13, $Button14, $Button15, $Button16, $Button17, $Button18, $Button19, $Button20, $Button21, $Button22, $Button23, $Button24, $Button25, $Button26, $Button27, $Button28, $Button29, $Button30, $Button31, $Button32, $Button33, $Button34, $Button35, $Button36, $Button37, $Button38, $Button39, $Button40, $Button41, $Button42, $Button43, $Button44, $Button45, $Button46, $Button47, $Button48, $Button49]
 
 
 
