d31ff19af4f2a53244ae1d881f4d2b74b4ab7ed9
ThrillerOfAutumn <thrillerofautumn@gmail.com>
2025-05-16 16:38:16 +0100

    BUTTON ARRAY DONE
    HOLY FFFFFFF that was painful.

--- 2_Project/RhythmQWERTY/Scenes/button.gd
+++ 2_Project/RhythmQWERTY/Scenes/button.gd
@@ -3,76 +3,7 @@ extends Node2D
 func _ready():
 	modulate.a = 0
 	fade_in()
-	var buttons_data = {
-		"button":		{"Letter":"H","Row":3,"Column":7},#
-		"button2":		{"Letter":"J","Row":3,"Column":8},#
-		"button3":		{"Letter":"G","Row":3,"Column":6},#
-		"button4":		{"Letter":"K","Row":3,"Column":9},#
-		"button5":		{"Letter":"M","Row":4,"Column":8},#
-		"button6":		{"Letter":"U","Row":2,"Column":8},#
-		"button7":		{"Letter":"N","Row":4,"Column":7},#
-		"button8":		{"Letter":"Y","Row":2,"Column":7},#
-		"button9":		{"Letter":"I","Row":2,"Column":9},#
-		"button10":		{"Letter":"B","Row":4,"Column":7},#
-		"button11":		{"Letter":",","Row":4,"Column":9},#
-		"button12":		{"Letter":"T","Row":2,"Column":7},
-		"button13":		{"Letter":"H","Row":3,"Column":7},
-		"button14":		{"Letter":"H","Row":3,"Column":7},
-		"button15":		{"Letter":"H","Row":3,"Column":7},
-		"button16":		{"Letter":"H","Row":3,"Column":7},
-		"button17":		{"Letter":"H","Row":3,"Column":7},
-		"button18":		{"Letter":"H","Row":3,"Column":7},
-		"button19":		{"Letter":"H","Row":3,"Column":7},
-		"button20":		{"Letter":"H","Row":3,"Column":7},
-		"button21":		{"Letter":"H","Row":3,"Column":7},
-		"button22":		{"Letter":"H","Row":3,"Column":7},
-		"button23":		{"Letter":"H","Row":3,"Column":7},
-		"button24":		{"Letter":"H","Row":3,"Column":7},
-		"button25":		{"Letter":"H","Row":3,"Column":7},
-		"button26":		{"Letter":"H","Row":3,"Column":7},
-		"button27":		{"Letter":"H","Row":3,"Column":7},
-		"button28":		{"Letter":"J","Row":3,"Column":8},
-		"button29":		{"Letter":"G","Row":3,"Column":6},
-		"button30":		{"Letter":"H","Row":3,"Column":7},
-		"button31":		{"Letter":"J","Row":3,"Column":8},
-		"button32":		{"Letter":"G","Row":3,"Column":6},
-		"button33":		{"Letter":"H","Row":3,"Column":7},
-		"button34":		{"Letter":"J","Row":3,"Column":8},
-		"button35":		{"Letter":"G","Row":3,"Column":6},
-		"button36":		{"Letter":"H","Row":3,"Column":7},
-		"button37":		{"Letter":"J","Row":3,"Column":8},
-		"button38":		{"Letter":"G","Row":3,"Column":6},
-		"button39":		{"Letter":"H","Row":3,"Column":7},
-		"button40":		{"Letter":"J","Row":3,"Column":8},
-		"button41":		{"Letter":"G","Row":3,"Column":6},
-		"button42":		{"Letter":"H","Row":3,"Column":7},
-		"button43":		{"Letter":"J","Row":3,"Column":8},
-		"button44":		{"Letter":"G","Row":3,"Column":6},
-		"button45":		{"Letter":"H","Row":3,"Column":7},
-		"button46":		{"Letter":"J","Row":3,"Column":8},
-		"button47":		{"Letter":"G","Row":3,"Column":6},
-		"button48":		{"Letter":"H","Row":3,"Column":7},
-		"button49":		{"Letter":"J","Row":3,"Column":8}}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	print(buttons_data["button2"]["Row"])
+
 		
 func fade_in():
 	var tween = create_tween()
--- 2_Project/RhythmQWERTY/Scripts/main_scene.gd
+++ 2_Project/RhythmQWERTY/Scripts/main_scene.gd
@@ -7,17 +7,82 @@ var button_instance = null
 @onready var marker = $Marker2D
 @onready var button_array = [$Button3, $Button4, $Button5, $Button6, $Button7, $Button8, $Button9, $Button10, $Button11, $Button12, $Button13, $Button14, $Button15, $Button16, $Button17, $Button18, $Button19, $Button20, $Button21, $Button22, $Button23, $Button24, $Button25, $Button26, $Button27, $Button28, $Button29, $Button30, $Button31, $Button32, $Button33, $Button34, $Button35, $Button36, $Button37, $Button38, $Button39, $Button40, $Button41, $Button42, $Button43, $Button44, $Button45, $Button46, $Button47, $Button48, $Button49]
 
-
+var buttons_data = {
+	"Button":		{"Letter":"H","Row":3,"Column":7},#
+	"Button2":		{"Letter":"J","Row":3,"Column":8},#
+	"Button3":		{"Letter":"G","Row":3,"Column":6},#
+	"Button4":		{"Letter":"K","Row":3,"Column":9},#
+	"Button5":		{"Letter":"M","Row":4,"Column":8},#
+	"Button6":		{"Letter":"U","Row":2,"Column":8},#
+	"Button7":		{"Letter":"N","Row":4,"Column":7},#
+	"Button8":		{"Letter":"Y","Row":2,"Column":7},#
+	"Button9":		{"Letter":"I","Row":2,"Column":9},#
+	"Button10":		{"Letter":"B","Row":4,"Column":6},#
+	"Button11":		{"Letter":",","Row":4,"Column":9},#
+	"Button12":		{"Letter":"T","Row":2,"Column":6},#
+	"Button13":		{"Letter":"F","Row":3,"Column":5},#
+	"Button14":		{"Letter":"L","Row":3,"Column":10},#
+	"Button15":		{"Letter":"O","Row":2,"Column":10},#
+	"Button16":		{"Letter":"V","Row":4,"Column":5},#
+	"Button17":		{"Letter":".","Row":4,"Column":10},#
+	"Button18":		{"Letter":"R","Row":2,"Column":5},#
+	"Button19":		{"Letter":"D","Row":3,"Column":4},#
+	"Button20":		{"Letter":"C","Row":4,"Column":4},#
+	"Button21":		{"Letter":"P","Row":2,"Column":11},#
+	"Button22":		{"Letter":";","Row":3,"Column":11},#
+	"Button23":		{"Letter":"7","Row":1,"Column":8},#
+	"Button24":		{"Letter":"6","Row":1,"Column":7},#
+	"Button25":		{"Letter":"8","Row":1,"Column":9},#
+	"Button26":		{"Letter":"9","Row":1,"Column":10},#
+	"Button27":		{"Letter":"/","Row":4,"Column":4},#
+	"Button28":		{"Letter":"Space","Row":5,"Column":6},#
+	"Button29":		{"Letter":"5","Row":1,"Column":6},#
+	"Button30":		{"Letter":"[","Row":2,"Column":12},#
+	"Button31":		{"Letter":"'","Row":3,"Column":12},#
+	"Button32":		{"Letter":"E","Row":2,"Column":4},#
+	"Button33":		{"Letter":"S","Row":3,"Column":3},#
+	"Button34":		{"Letter":"0","Row":1,"Column":11},#
+	"Button35":		{"Letter":"X","Row":4,"Column":3},
+	"Button36":		{"Letter":"4","Row":1,"Column":5},
+	"Button37":		{"Letter":"3","Row":1,"Column":4},
+	"Button38":		{"Letter":"W","Row":2,"Column":3},
+	"Button39":		{"Letter":"A","Row":3,"Column":2},
+	"Button40":		{"Letter":"Z","Row":4,"Column":2},
+	"Button41":		{"Letter":"_","Row":1,"Column":12},
+	"Button42":		{"Letter":"=","Row":1,"Column":13},
+	"Button43":		{"Letter":"]","Row":2,"Column":13},
+	"Button44":		{"Letter":"#","Row":3,"Column":13},
+	"Button45":		{"Letter":"2","Row":1,"Column":3},
+	"Button46":		{"Letter":"Q","Row":2,"Column":2},
+	"Button47":		{"Letter":"Backslash","Row":4,"Column":1},
+	"Button48":		{"Letter":"1","Row":1,"Column":2},
+	"Button49":		{"Letter":"Enter","Row":2,"Column":14}}
+	
 
 
 func _ready():
 	start_timer()
 
 
+
+## Popup Menu Function
 func _input(event):
 	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_ESCAPE:
 		toggle_popup()
+		
+## Key press detection event
+	if event is InputEventKey and event.is_pressed():
+		var key_pressed = OS.get_keycode_string(event.keycode)
+		check_buttons(key_pressed)
+		print("This is the button you pressed:", key_pressed)
 
+func check_buttons(key_pressed):
+
+	for button_name in buttons_data.keys():
+		if buttons_data[button_name]["Letter"] == key_pressed:
+			var button_node = get_node(button_name)
+			if button_node:
+				button_node.visible = !button_node.visible
 
 
 
@@ -33,11 +98,10 @@ func toggle_popup():
 		popup_instance.global_position = get_viewport_rect().size / 2 - popup_instance.size / 2
 		
 		
-		
 ## timer for adding new buttons every 5 seconds and incrementing the gamestate by 1
 func start_timer():
 	var timer = Timer.new()
-	timer.wait_time = 2
+	timer.wait_time = 1
 	timer.one_shot = false
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 	add_child(timer)
@@ -48,8 +112,8 @@ func _on_timer_timeout():
 	#add_child(button_instance)
 	#button_instance.global_position = marker.global_position
 	button_array[global.gamestep].show()
-	global.gamestep += 1
-
-
+	if global.gamestep >= 46:
+		pass
+	else: global.gamestep += 1
 	
 	
