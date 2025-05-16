baa512aaa6d5f6969b0011952dac2fe6678cacd4
ThrillerOfAutumn <thrillerofautumn@gmail.com>
2025-05-16 15:04:20 +0100

    rows and columns planning

--- 2_Project/RhythmQWERTY/Scenes/button.gd
+++ 2_Project/RhythmQWERTY/Scenes/button.gd
@@ -7,3 +7,11 @@ func _ready():
 func fade_in():
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 2.5)
+
+
+when q is pressed
+button 46 activated
+
+
+button X activated?
+clear X status and get it ready to charge
