2c724e2a616fd4bee4e16359814fcb8d587b1978
hedgemandan <159487488+hedgemandan@users.noreply.github.com>
2025-05-17 14:22:34 +0100

    Button colours added to dictionary

--- 2_Project/RhythmQWERTY/Scripts/main_scene.gd
+++ 2_Project/RhythmQWERTY/Scripts/main_scene.gd
@@ -18,60 +18,62 @@ var bufferOGcolour
 @onready var keys_unlocked = $"Keys Unlocked"
 
 var buttons_data = {
-	"Button":		{"Letter":"H","Row":3,"Column":7},#
-	"Button2":		{"Letter":"J","Row":3,"Column":8},#
-	"Button3":		{"Letter":"G","Row":3,"Column":6},#
-	"Button4":		{"Letter":"K","Row":3,"Column":9},#
-	"Button5":		{"Letter":"M","Row":4,"Column":8},#
-	"Button6":		{"Letter":"U","Row":2,"Column":8},#
-	"Button7":		{"Letter":"N","Row":4,"Column":7},#
-	"Button8":		{"Letter":"Y","Row":2,"Column":7},#
-	"Button9":		{"Letter":"I","Row":2,"Column":9},#
-	"Button10":		{"Letter":"B","Row":4,"Column":6},#
-	"Button11":		{"Letter":"Comma","Row":4,"Column":9},#
-	"Button12":		{"Letter":"T","Row":2,"Column":6},#
-	"Button13":		{"Letter":"F","Row":3,"Column":5},#
-	"Button14":		{"Letter":"L","Row":3,"Column":10},#
-	"Button15":		{"Letter":"O","Row":2,"Column":10},#
-	"Button16":		{"Letter":"V","Row":4,"Column":5},#
-	"Button17":		{"Letter":"Period","Row":4,"Column":10},#
-	"Button18":		{"Letter":"R","Row":2,"Column":5},#
-	"Button19":		{"Letter":"D","Row":3,"Column":4},#
-	"Button20":		{"Letter":"C","Row":4,"Column":4},#
-	"Button21":		{"Letter":"P","Row":2,"Column":11},#
-	"Button22":		{"Letter":"Semicolon","Row":3,"Column":11},#
-	"Button23":		{"Letter":"7","Row":1,"Column":8},#
-	"Button24":		{"Letter":"6","Row":1,"Column":7},#
-	"Button25":		{"Letter":"8","Row":1,"Column":9},#
-	"Button26":		{"Letter":"9","Row":1,"Column":10},#
-	"Button27":		{"Letter":"Slash","Row":4,"Column":4},#
-	"Button28":		{"Letter":"Space","Row":5,"Column":6},#
-	"Button29":		{"Letter":"5","Row":1,"Column":6},#
-	"Button30":		{"Letter":"BracketLeft","Row":2,"Column":12},#
-	"Button31":		{"Letter":"Apostrophe","Row":3,"Column":12},#
-	"Button32":		{"Letter":"E","Row":2,"Column":4},#
-	"Button33":		{"Letter":"S","Row":3,"Column":3},#
-	"Button34":		{"Letter":"0","Row":1,"Column":11},#
-	"Button35":		{"Letter":"X","Row":4,"Column":3},
-	"Button36":		{"Letter":"4","Row":1,"Column":5},
-	"Button37":		{"Letter":"3","Row":1,"Column":4},
-	"Button38":		{"Letter":"W","Row":2,"Column":3},
-	"Button39":		{"Letter":"A","Row":3,"Column":2},
-	"Button40":		{"Letter":"Z","Row":4,"Column":2},
-	"Button41":		{"Letter":"Minus","Row":1,"Column":12},
-	"Button42":		{"Letter":"Equal","Row":1,"Column":13},
-	"Button43":		{"Letter":"BracketRight","Row":2,"Column":13},
-	"Button44":		{"Letter":"NumberSign","Row":3,"Column":13},
-	"Button45":		{"Letter":"2","Row":1,"Column":3},
-	"Button46":		{"Letter":"Q","Row":2,"Column":2},
-	"Button47":		{"Letter":"BackSlash","Row":4,"Column":1},
-	"Button48":		{"Letter":"1","Row":1,"Column":2},
-	"Button49":		{"Letter":"Enter","Row":2,"Column":14}}
+	"Button":		{"Letter":"H","Row":3,"Column":7,"Color":Color(0, 1, 1,1)},#
+	"Button2":		{"Letter":"J","Row":3,"Column":8,"Color":Color(0, 1, 1,1)},#
+	"Button3":		{"Letter":"G","Row":3,"Column":6,"Color":Color(0.251, 0.773, 1,1)},#
+	"Button4":		{"Letter":"K","Row":3,"Column":9,"Color":Color(0.251, 0.824, 1,1)},#
+	"Button5":		{"Letter":"M","Row":4,"Column":8,"Color":Color(0.4, 0.714, 1,1)},#
+	"Button6":		{"Letter":"U","Row":2,"Column":8,"Color":Color(0.4, 0.616, 1,1)},#
+	"Button7":		{"Letter":"N","Row":4,"Column":7,"Color":Color(0.4, 0.616, 1,1)},#
+	"Button8":		{"Letter":"Y","Row":2,"Column":7,"Color":Color(0.451, 0.596, 1,1)},#
+	"Button9":		{"Letter":"I","Row":2,"Column":9,"Color":Color(0.502, 0.549, 1,1)},#
+	"Button10":		{"Letter":"B","Row":4,"Column":6,"Color":Color(0.686, 0.314, 1,1)},#
+	"Button11":		{"Letter":"Comma","Row":4,"Column":9,"Color":Color(0.616, 0.251, 1,1)},#
+	"Button12":		{"Letter":"T","Row":2,"Column":6,"Color":Color(0.725, 0.2, 1,1)},#
+	"Button13":		{"Letter":"F","Row":3,"Column":5,"Color":Color(0.741, 0.251, 1,1)},#
+	"Button14":		{"Letter":"L","Row":3,"Column":10,"Color":Color(0.557, 0.353, 1,1)},#
+	"Button15":		{"Letter":"O","Row":2,"Column":10,"Color":Color(0.694, 0.302, 1,1)},#
+	"Button16":		{"Letter":"V","Row":4,"Column":5,"Color":Color(0.973, 0.027, 1,1)},#
+	"Button17":		{"Letter":"Period","Row":4,"Column":10,"Color":Color(1, 0, 1,1)},#
+	"Button18":		{"Letter":"R","Row":2,"Column":5,"Color":Color(1, 0.102, 0.937,1)},#
+	"Button19":		{"Letter":"D","Row":3,"Column":4,"Color":Color(1, 0.102, 0.937,1)},#
+	"Button20":		{"Letter":"C","Row":4,"Column":4,"Color":Color(1, 0.173, 0.745,1)},#
+	"Button21":		{"Letter":"P","Row":2,"Column":11,"Color":Color(1, 0.051, 1,1)},#
+	"Button22":		{"Letter":"Semicolon","Row":3,"Column":11,"Color":Color(1, 0, 1,1)},#
+	"Button23":		{"Letter":"7","Row":1,"Column":8,"Color":Color(1, 0.153, 0.8,1)},#
+	"Button24":		{"Letter":"6","Row":1,"Column":7,"Color":Color(1, 0.2, 0.757,1)},#
+	"Button25":		{"Letter":"8","Row":1,"Column":9,"Color":Color(1, 0.153, 0.8,1)},#
+	"Button26":		{"Letter":"9","Row":1,"Column":10,"Color":Color(1, 0.251, 0.698,1)},#
+	"Button27":		{"Letter":"Slash","Row":4,"Column":11,"Color":Color(1, 0.2, 0.757,1)},#
+	"Button28":		{"Letter":"Space","Row":5,"Column":6,"Color":Color(1, 0.282, 0.58,1)},#
+	"Button29":		{"Letter":"5","Row":1,"Column":6,"Color":Color(1, 0.302, 0.553,1)},#
+	"Button30":		{"Letter":"BracketLeft","Row":2,"Column":12,"Color":Color(1, 0.302, 0.671,1)},#
+	"Button31":		{"Letter":"Apostrophe","Row":3,"Column":12,"Color":Color(1, 0.251, 0.643,1)},#
+	"Button32":		{"Letter":"E","Row":2,"Column":4,"Color":Color(1, 0.302, 0.604,1)},#
+	"Button33":		{"Letter":"S","Row":3,"Column":3,"Color":Color(1, 0.302, 0.486,1)},#
+	"Button34":		{"Letter":"0","Row":1,"Column":11,"Color":Color(1, 0.4, 0.514,1)},#
+	"Button35":		{"Letter":"X","Row":4,"Column":3,"Color":Color(1, 0.38, 0.435,1)},#
+	"Button36":		{"Letter":"4","Row":1,"Column":5,"Color":Color(1, 0.416, 0.302,1)},#
+	"Button37":		{"Letter":"3","Row":1,"Column":4,"Color":Color(1, 0.592, 0.051,1)},#
+	"Button38":		{"Letter":"W","Row":2,"Column":3,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button39":		{"Letter":"A","Row":3,"Column":2,"Color":Color(1, 0.576, 0.153,1)},#
+	"Button40":		{"Letter":"Z","Row":4,"Column":2,"Color":Color(1, 0.596, 0.106,1)},#
+	"Button41":		{"Letter":"Minus","Row":1,"Column":12,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button42":		{"Letter":"Equal","Row":1,"Column":13,"Color":Color(1, 0.667, 0,1)},#
+	"Button43":		{"Letter":"BracketRight","Row":2,"Column":13,"Color":Color(1, 0.467, 0.302,1)},#
+	"Button44":		{"Letter":"NumberSign","Row":3,"Column":13,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button45":		{"Letter":"2","Row":1,"Column":3,"Color":Color(1, 0.831, 0,1)},#
+	"Button46":		{"Letter":"Q","Row":2,"Column":2,"Color":Color(1, 0.769, 0.043,1)},#
+	"Button47":		{"Letter":"BackSlash","Row":4,"Column":1,"Color":Color(1, 0.871, 0.024,1)},#
+	"Button48":		{"Letter":"1","Row":1,"Column":2,"Color":Color(1, 1, 0,1)},#
+	"Button49":		{"Letter":"Enter","Row":2,"Column":14,"Color":Color(1, 0.831, 0,1)}}#
 
 func _ready():
 	start_newbuttonspawn_timer()
 	start_buttoncharge_timer()
 	Music.play()
+	for n in buttons_data.length:
+		pass
 
 
 ## Popup Menu Function
