c5372a9e598d94f62115d4aaf5d66c2242612628
ThrillerOfAutumn <thrillerofautumn@gmail.com>
2025-05-17 17:31:40 +0100

    dfg

--- 2_Project/RhythmQWERTY/Scripts/main_scene.gd
+++ 2_Project/RhythmQWERTY/Scripts/main_scene.gd
@@ -16,56 +16,56 @@ var bufferOGcolour
 @onready var incorrect_clicks = $"Incorrect Clicks"
 @onready var keys_unlocked = $"Keys Unlocked"
 
-var buttons_data = {
-	"Button":		{"Letter":"H","Row":3,"Column":7,"Color":Color(0, 1, 1,1)},#
-	"Button2":		{"Letter":"J","Row":3,"Column":8,"Color":Color(0, 1, 1,1)},#
-	"Button3":		{"Letter":"G","Row":3,"Column":6,"Color":Color(0.251, 0.773, 1,1)},#
-	"Button4":		{"Letter":"K","Row":3,"Column":9,"Color":Color(0.251, 0.824, 1,1)},#
-	"Button5":		{"Letter":"M","Row":4,"Column":8,"Color":Color(0.4, 0.714, 1,1)},#
-	"Button6":		{"Letter":"U","Row":2,"Column":8,"Color":Color(0.4, 0.616, 1,1)},#
-	"Button7":		{"Letter":"N","Row":4,"Column":7,"Color":Color(0.4, 0.616, 1,1)},#
-	"Button8":		{"Letter":"Y","Row":2,"Column":7,"Color":Color(0.451, 0.596, 1,1)},#
-	"Button9":		{"Letter":"I","Row":2,"Column":9,"Color":Color(0.502, 0.549, 1,1)},#
-	"Button10":		{"Letter":"B","Row":4,"Column":6,"Color":Color(0.686, 0.314, 1,1)},#
-	"Button11":		{"Letter":"Comma","Row":4,"Column":9,"Color":Color(0.616, 0.251, 1,1)},#
-	"Button12":		{"Letter":"T","Row":2,"Column":6,"Color":Color(0.725, 0.2, 1,1)},#
-	"Button13":		{"Letter":"F","Row":3,"Column":5,"Color":Color(0.741, 0.251, 1,1)},#
-	"Button14":		{"Letter":"L","Row":3,"Column":10,"Color":Color(0.557, 0.353, 1,1)},#
-	"Button15":		{"Letter":"O","Row":2,"Column":10,"Color":Color(0.694, 0.302, 1,1)},#
-	"Button16":		{"Letter":"V","Row":4,"Column":5,"Color":Color(0.973, 0.027, 1,1)},#
-	"Button17":		{"Letter":"Period","Row":4,"Column":10,"Color":Color(1, 0, 1,1)},#
-	"Button18":		{"Letter":"R","Row":2,"Column":5,"Color":Color(1, 0.102, 0.937,1)},#
-	"Button19":		{"Letter":"D","Row":3,"Column":4,"Color":Color(1, 0.102, 0.937,1)},#
-	"Button20":		{"Letter":"C","Row":4,"Column":4,"Color":Color(1, 0.173, 0.745,1)},#
-	"Button21":		{"Letter":"P","Row":2,"Column":11,"Color":Color(1, 0.051, 1,1)},#
-	"Button22":		{"Letter":"Semicolon","Row":3,"Column":11,"Color":Color(1, 0, 1,1)},#
-	"Button23":		{"Letter":"7","Row":1,"Column":8,"Color":Color(1, 0.153, 0.8,1)},#
-	"Button24":		{"Letter":"6","Row":1,"Column":7,"Color":Color(1, 0.2, 0.757,1)},#
-	"Button25":		{"Letter":"8","Row":1,"Column":9,"Color":Color(1, 0.153, 0.8,1)},#
-	"Button26":		{"Letter":"9","Row":1,"Column":10,"Color":Color(1, 0.251, 0.698,1)},#
-	"Button27":		{"Letter":"Slash","Row":4,"Column":11,"Color":Color(1, 0.2, 0.757,1)},#
-	"Button28":		{"Letter":"Space","Row":5,"Column":6,"Color":Color(1, 0.282, 0.58,1)},#
-	"Button29":		{"Letter":"5","Row":1,"Column":6,"Color":Color(1, 0.302, 0.553,1)},#
-	"Button30":		{"Letter":"BracketLeft","Row":2,"Column":12,"Color":Color(1, 0.302, 0.671,1)},#
-	"Button31":		{"Letter":"Apostrophe","Row":3,"Column":12,"Color":Color(1, 0.251, 0.643,1)},#
-	"Button32":		{"Letter":"E","Row":2,"Column":4,"Color":Color(1, 0.302, 0.604,1)},#
-	"Button33":		{"Letter":"S","Row":3,"Column":3,"Color":Color(1, 0.302, 0.486,1)},#
-	"Button34":		{"Letter":"0","Row":1,"Column":11,"Color":Color(1, 0.4, 0.514,1)},#
-	"Button35":		{"Letter":"X","Row":4,"Column":3,"Color":Color(1, 0.38, 0.435,1)},#
-	"Button36":		{"Letter":"4","Row":1,"Column":5,"Color":Color(1, 0.416, 0.302,1)},#
-	"Button37":		{"Letter":"3","Row":1,"Column":4,"Color":Color(1, 0.592, 0.051,1)},#
-	"Button38":		{"Letter":"W","Row":2,"Column":3,"Color":Color(1, 0.502, 0.251,1)},#
-	"Button39":		{"Letter":"A","Row":3,"Column":2,"Color":Color(1, 0.576, 0.153,1)},#
-	"Button40":		{"Letter":"Z","Row":4,"Column":2,"Color":Color(1, 0.596, 0.106,1)},#
-	"Button41":		{"Letter":"Minus","Row":1,"Column":12,"Color":Color(1, 0.502, 0.251,1)},#
-	"Button42":		{"Letter":"Equal","Row":1,"Column":13,"Color":Color(1, 0.667, 0,1)},#
-	"Button43":		{"Letter":"BracketRight","Row":2,"Column":13,"Color":Color(1, 0.467, 0.302,1)},#
-	"Button44":		{"Letter":"NumberSign","Row":3,"Column":13,"Color":Color(1, 0.502, 0.251,1)},#
-	"Button45":		{"Letter":"2","Row":1,"Column":3,"Color":Color(1, 0.831, 0,1)},#
-	"Button46":		{"Letter":"Q","Row":2,"Column":2,"Color":Color(1, 0.769, 0.043,1)},#
-	"Button47":		{"Letter":"BackSlash","Row":4,"Column":1,"Color":Color(1, 0.871, 0.024,1)},#
-	"Button48":		{"Letter":"1","Row":1,"Column":2,"Color":Color(1, 1, 0,1)},#
-	"Button49":		{"Letter":"Enter","Row":2,"Column":14,"Color":Color(1, 0.831, 0,1)}}#
+@onready var buttons_data = {
+	"Button1":		{"Node":$AllButtons/Button,"Letter":"H","Row":3,"Column":7,"Color":Color(0, 1, 1,1)},#
+	"Button2":		{"Node":$AllButtons/Button2,"Letter":"J","Row":3,"Column":8,"Color":Color(0, 1, 1,1)},#
+	"Button3":		{"Node":$AllButtons/Button3,"Letter":"G","Row":3,"Column":6,"Color":Color(0.251, 0.773, 1,1)},#
+	"Button4":		{"Node":$AllButtons/Button4,"Letter":"K","Row":3,"Column":9,"Color":Color(0.251, 0.824, 1,1)},#
+	"Button5":		{"Node":$AllButtons/Button5,"Letter":"M","Row":4,"Column":8,"Color":Color(0.4, 0.714, 1,1)},#
+	"Button6":		{"Node":$AllButtons/Button6,"Letter":"U","Row":2,"Column":8,"Color":Color(0.4, 0.616, 1,1)},#
+	"Button7":		{"Node":$AllButtons/Button7,"Letter":"N","Row":4,"Column":7,"Color":Color(0.4, 0.616, 1,1)},#
+	"Button8":		{"Node":$AllButtons/Button8,"Letter":"Y","Row":2,"Column":7,"Color":Color(0.451, 0.596, 1,1)},#
+	"Button9":		{"Node":$AllButtons/Button9,"Letter":"I","Row":2,"Column":9,"Color":Color(0.502, 0.549, 1,1)},#
+	"Button10":		{"Node":$AllButtons/Button10,"Letter":"B","Row":4,"Column":6,"Color":Color(0.686, 0.314, 1,1)},#
+	"Button11":		{"Node":$AllButtons/Button11,"Letter":"Comma","Row":4,"Column":9,"Color":Color(0.616, 0.251, 1,1)},#
+	"Button12":		{"Node":$AllButtons/Button12,"Letter":"T","Row":2,"Column":6,"Color":Color(0.725, 0.2, 1,1)},#
+	"Button13":		{"Node":$AllButtons/Button13,"Letter":"F","Row":3,"Column":5,"Color":Color(0.741, 0.251, 1,1)},#
+	"Button14":		{"Node":$AllButtons/Button14,"Letter":"L","Row":3,"Column":10,"Color":Color(0.557, 0.353, 1,1)},#
+	"Button15":		{"Node":$AllButtons/Button15,"Letter":"O","Row":2,"Column":10,"Color":Color(0.694, 0.302, 1,1)},#
+	"Button16":		{"Node":$AllButtons/Button16,"Letter":"V","Row":4,"Column":5,"Color":Color(0.973, 0.027, 1,1)},#
+	"Button17":		{"Node":$AllButtons/Button17,"Letter":"Period","Row":4,"Column":10,"Color":Color(1, 0, 1,1)},#
+	"Button18":		{"Node":$AllButtons/Button18,"Letter":"R","Row":2,"Column":5,"Color":Color(1, 0.102, 0.937,1)},#
+	"Button19":		{"Node":$AllButtons/Button19,"Letter":"D","Row":3,"Column":4,"Color":Color(1, 0.102, 0.937,1)},#
+	"Button20":		{"Node":$AllButtons/Button20,"Letter":"C","Row":4,"Column":4,"Color":Color(1, 0.173, 0.745,1)},#
+	"Button21":		{"Node":$AllButtons/Button21,"Letter":"P","Row":2,"Column":11,"Color":Color(1, 0.051, 1,1)},#
+	"Button22":		{"Node":$AllButtons/Button22,"Letter":"Semicolon","Row":3,"Column":11,"Color":Color(1, 0, 1,1)},#
+	"Button23":		{"Node":$AllButtons/Button23,"Letter":"7","Row":1,"Column":8,"Color":Color(1, 0.153, 0.8,1)},#
+	"Button24":		{"Node":$AllButtons/Button24,"Letter":"6","Row":1,"Column":7,"Color":Color(1, 0.2, 0.757,1)},#
+	"Button25":		{"Node":$AllButtons/Button25,"Letter":"8","Row":1,"Column":9,"Color":Color(1, 0.153, 0.8,1)},#
+	"Button26":		{"Node":$AllButtons/Button26,"Letter":"9","Row":1,"Column":10,"Color":Color(1, 0.251, 0.698,1)},#
+	"Button27":		{"Node":$AllButtons/Button27,"Letter":"Slash","Row":4,"Column":11,"Color":Color(1, 0.2, 0.757,1)},#
+	"Button28":		{"Node":$AllButtons/Button28,"Letter":"Space","Row":5,"Column":6,"Color":Color(1, 0.282, 0.58,1)},#
+	"Button29":		{"Node":$AllButtons/Button29,"Letter":"5","Row":1,"Column":6,"Color":Color(1, 0.302, 0.553,1)},#
+	"Button30":		{"Node":$AllButtons/Button30,"Letter":"BracketLeft","Row":2,"Column":12,"Color":Color(1, 0.302, 0.671,1)},#
+	"Button31":		{"Node":$AllButtons/Button31,"Letter":"Apostrophe","Row":3,"Column":12,"Color":Color(1, 0.251, 0.643,1)},#
+	"Button32":		{"Node":$AllButtons/Button32,"Letter":"E","Row":2,"Column":4,"Color":Color(1, 0.302, 0.604,1)},#
+	"Button33":		{"Node":$AllButtons/Button33,"Letter":"S","Row":3,"Column":3,"Color":Color(1, 0.302, 0.486,1)},#
+	"Button34":		{"Node":$AllButtons/Button34,"Letter":"0","Row":1,"Column":11,"Color":Color(1, 0.4, 0.514,1)},#
+	"Button35":		{"Node":$AllButtons/Button35,"Letter":"X","Row":4,"Column":3,"Color":Color(1, 0.38, 0.435,1)},#
+	"Button36":		{"Node":$AllButtons/Button36,"Letter":"4","Row":1,"Column":5,"Color":Color(1, 0.416, 0.302,1)},#
+	"Button37":		{"Node":$AllButtons/Button37,"Letter":"3","Row":1,"Column":4,"Color":Color(1, 0.592, 0.051,1)},#
+	"Button38":		{"Node":$AllButtons/Button38,"Letter":"W","Row":2,"Column":3,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button39":		{"Node":$AllButtons/Button39,"Letter":"A","Row":3,"Column":2,"Color":Color(1, 0.576, 0.153,1)},#
+	"Button40":		{"Node":$AllButtons/Button40,"Letter":"Z","Row":4,"Column":2,"Color":Color(1, 0.596, 0.106,1)},#
+	"Button41":		{"Node":$AllButtons/Button41,"Letter":"Minus","Row":1,"Column":12,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button42":		{"Node":$AllButtons/Button42,"Letter":"Equal","Row":1,"Column":13,"Color":Color(1, 0.667, 0,1)},#
+	"Button43":		{"Node":$AllButtons/Button43,"Letter":"BracketRight","Row":2,"Column":13,"Color":Color(1, 0.467, 0.302,1)},#
+	"Button44":		{"Node":$AllButtons/Button44,"Letter":"NumberSign","Row":3,"Column":13,"Color":Color(1, 0.502, 0.251,1)},#
+	"Button45":		{"Node":$AllButtons/Button45,"Letter":"2","Row":1,"Column":3,"Color":Color(1, 0.831, 0,1)},#
+	"Button46":		{"Node":$AllButtons/Button46,"Letter":"Q","Row":2,"Column":2,"Color":Color(1, 0.769, 0.043,1)},#
+	"Button47":		{"Node":$AllButtons/Button47,"Letter":"BackSlash","Row":4,"Column":1,"Color":Color(1, 0.871, 0.024,1)},#
+	"Button48":		{"Node":$AllButtons/Button48,"Letter":"1","Row":1,"Column":2,"Color":Color(1, 1, 0,1)},#
+	"Button49":		{"Node":$AllButtons/Button49,"Letter":"Enter","Row":2,"Column":14,"Color":Color(1, 0.831, 0,1)}}#
 
 
 func _ready():
@@ -77,7 +77,7 @@ func _ready():
 		#print("I have interated this many times:", n)
 		#pass
 	for button_name in buttons_data.keys():
-		var button_node = $AllButtons.get_node(button_name)
+		var button_node = buttons_data[button_name]["Node"]
 		if button_node:
 			button_node.modulate = buttons_data[button_name]["Color"]
 			print("Applied color to:", button_name)
@@ -102,7 +102,7 @@ func _input(event):
 func check_buttons(key_pressed):
 	for button_name in buttons_data.keys():
 		if buttons_data[button_name]["Letter"] == key_pressed:
-			var button_node = get_node("AllButtons/" + button_name)
+			var button_node = buttons_data[button_name]["Node"]
 			if button_node.visible:
 				button_node.scale = Vector2(1, 1)
 				#if bufferOGcolour:
@@ -149,7 +149,7 @@ func toggle_popup():
 ## timer for adding new buttons every 5 seconds and incrementing the gamestate by 1
 func start_newbuttonspawn_timer():
 	var timer = Timer.new()
-	timer.wait_time = 3
+	timer.wait_time = 2
 	timer.one_shot = false
 	timer.connect("timeout", Callable(self, "_on_timer_timeoutNB"))
 	add_child(timer)
@@ -159,7 +159,10 @@ func _on_timer_timeoutNB():
 	if global.gamestep >= 47:
 		pass
 	else: 
-		button_array[global.gamestep + 2].show()
+		#var adjustedgamestep = (global.gamestep + 2)
+		var gamestepstring = "Button" + str(global.gamestep + 2) #concatenate
+		buttons_data[gamestepstring]["Node"].show()
+		#button_array[global.gamestep + 2].show()
 		global.numberoffbuttonsvisible += 1
 		keys_unlocked.text = str(global.numberoffbuttonsvisible)
 		global.gamestep += 1
@@ -170,7 +173,7 @@ func _on_timer_timeoutNB():
 ## timer for making a random button go green from the selection of buttons which are visible
 func start_buttoncharge_timer():
 	var timer = Timer.new()
-	timer.wait_time = 2 - (global.gamestep)*0.1
+	timer.wait_time = 1 - (global.gamestep)*0.1
 	timer.one_shot = false
 	timer.connect("timeout", Callable(self, "_on_timer_timeoutBC"))
 	add_child(timer)
@@ -181,51 +184,28 @@ func _on_timer_timeoutBC():
 		
 		#if/conditions like gamestate are met then highlight more than 1 random button. Then three adjacent horizontal. Then 2 adjacent vertical. 
 
-		## This immediate paragraph below allows for two buttons charging at the same time beyond gamestep 10
+		## This immediate paragraph below allows for two buttons charging at the same time beyond gamestep 12
 		if global.gamestep >= 12: #enable doubles
 			var spawnlimit = 2
 			for n in spawnlimit:
-				var random_button = button_array[randi_range(0, global.numberoffbuttonsvisible -1)]
+				var holdingvar1 = "Button" + str(randi_range(1, global.numberoffbuttonsvisible -1)) #concatenate
+				var random_button = buttons_data[holdingvar1]["Node"]
 				random_button.grow_anim()
 				if random_button in global.clickablebuttons:
 					pass	#Do not double up charging the same button!
 				else:
 					global.clickablebuttons.append(random_button)
 		## Paired buttons?
-		elif global.gamestep >= 8:
-			var random_button = button_array[randi_range(0, global.numberoffbuttonsvisible -1)]
-			for button_name in buttons_data.keys():
-			var button_node = $AllButtons.get_node(button_name)
-			
-			
-			
-			var temporaryrow = null
-			var temporarycolumn = null
-			var adjacent_button = null
-			for button_name in buttons_data:
-				random_button = $AllButtons.get_node(button_name)
-				if button_name.name == random_button:
-					temporaryrow = buttons_data[button_name]["Row"]
-					temporarycolumn = buttons_data[button_name]["Column"]
-			print("this is the row of the random button:", temporaryrow)
-			print("this is the column of the random button:", temporarycolumn)
-			# check random_button, get random_button name, and row
-			# check buttons_data for nodes with same row number and only +/- 1 column number difference
-			# pick one of two nodes and charge that one as well
-			#
-	#for button_name in buttons_data.keys():
-		#var button_node = $AllButtons.get_node(button_name)
-		#if button_node:
-			#button_node.modulate = buttons_data[button_name]["Color"]
-			#print("Applied color to:", button_name)
-			
-			
-			
-			random_button.grow_anim()
-			if random_button in global.clickablebuttons:
-				pass	#Do not double up charging the same button!
-			else:
-				global.clickablebuttons.append(random_button)
+		elif global.gamestep >= 12: #enable doubles
+			var spawnlimitadj = 2
+			for n in spawnlimitadj:
+				var holdingvar2 = "Button" + str(randi_range(1, global.numberoffbuttonsvisible -1)) #concatenate
+				var random_button = buttons_data[holdingvar2]["Node"]
+				random_button.grow_anim()
+				if random_button in global.clickablebuttons:
+					pass	#Do not double up charging the same button!
+				else:
+					global.clickablebuttons.append(random_button)
 		## This immediate paragraph allows only one button to charging at a time prior to gamestep 10
 		else:
 			var random_button = button_array[randi_range(0, global.numberoffbuttonsvisible -1)]
